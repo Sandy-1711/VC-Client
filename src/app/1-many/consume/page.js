1f72bf9822c3dee4f8ed92710c9c70b97efbfb5b
Sandy-1711 <sandy1711003@gmail.com>
2024-08-06 18:10:10 +0530

    1tomany

---
+++ src/app/1-many/consume/page.js
@@ -0,0 +1,60 @@
+'use client';
+
+import { useEffect } from "react";
+
+export default function Consumer() {
+    const call = async () => {
+        const pc = new RTCPeerConnection({
+            iceServers: [{ urls: 'stun:stun.l.google.com:19302' }]
+        });
+
+        pc.addTransceiver('video', { direction: 'recvonly' });
+
+        pc.onnegotiationneeded = async () => {
+            try {
+                await handleNegotiationNeededEvent(pc);
+            } catch (error) {
+                console.error('Error during negotiation:', error);
+            }
+        };
+
+        pc.ontrack = (event) => {
+            console.log('Track event:', event);
+            document.getElementById('remoteStream').srcObject = event.streams[0];
+        };
+
+        console.log('PeerConnection initialized:', pc);
+    };
+
+    async function handleNegotiationNeededEvent(pc) {
+        const offer = await pc.createOffer();
+        await pc.setLocalDescription(offer);
+        const res = await fetch('http://localhost:4000/api/v1/consume', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(offer)
+        });
+
+        if (!res.ok) {
+            throw new Error('Failed to fetch the SDP from the server');
+        }
+
+        const data = await res.json();
+        console.log(data.senderStream);
+        
+        const answer = new RTCSessionDescription(data.payload.sdp);
+        await pc.setRemoteDescription(answer);
+    }
+
+    useEffect(() => {
+        call().catch(console.error);
+    }, []);
+
+    return (
+        <div className="w-screen h-[100dvh] bg-white">
+            <video id="remoteStream" className="w-[500px] h-[500px]" playsInline muted autoPlay></video>
+        </div>
+    );
+}
---
+++ src/app/1-many/page.js
@@ -0,0 +1,48 @@
+'use client'
+import { useRef, useState } from "react"
+
+export default function OneMany() {
+    const localStreamRef = useRef(null);
+    const remoteStreamRef = useRef(null);
+    const [pc, setPc] = useState(null);
+    async function call() {
+        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+            const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+            console.log(stream);
+            document.getElementById('localVideo').srcObject = stream;
+            const pc = new RTCPeerConnection({
+                iceServers: [{ urls: 'stun:stun.l.google.com:19302' }]
+            });
+            setPc(pc);
+            stream.getTracks().forEach((track) => {
+                pc.addTrack(track, stream)
+            });
+            // pc.addStream(stream);
+            pc.onnegotiationneeded = () => handleNegotiationNeededEvent(pc);
+            console.log(pc);
+        }
+        else {
+            alert('getUserMedia not supported');
+        }
+    }
+
+    async function handleNegotiationNeededEvent(pc) {
+        const offer = await pc.createOffer()
+        pc.setLocalDescription(offer);
+        const res = await fetch('http://localhost:4000/api/v1/broadcast', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(offer)
+        })
+        const data = await res.json();
+        const answer = data.sdp;
+        const desc = new RTCSessionDescription(answer);
+        pc.setRemoteDescription(desc);
+    }
+    return <div className="w-full h-[100dvh] bg-white text-[#2c2c2c] justify-center items-center flex-col flex">
+        <button className="bg-blue-500 hover:bg-blue-700 transition-colors text-white px-3 py-1.5 rounded" onClick={call}>Call</button>
+        <video id="localVideo" className="h-[500px] aspect-video" autoPlay muted></video>
+    </div>
+}
